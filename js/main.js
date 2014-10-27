8d10f1750f43eced45efa518048e73d31fc38c9d
Lucas Pirola <lucaspirola@gmail.com>
2014-10-27 18:16:12 -0200

    adicionado slice de apoiadores; adicionado logica para cadastro de apoiadores;

--- js/main.js
+++ js/main.js
@@ -31,4 +31,28 @@
       });
     }
   });
+
+  var myDataRef = new Firebase('https://jgqia35uk80.firebaseio-demo.com/');
+
+  $('#emailInput').keypress(function (e) {
+    if (e.keyCode == 13) {
+      var name = $('#nameInput').val();
+      var email = $('#emailInput').val();
+
+      myDataRef.push({name: name, email: email});
+
+      $('#nameInput').val('');
+      $('#emailInput').val('');
+    }
+  });
+
+   myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.email);
+  });
+
+  function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  };
 })(jQuery);
